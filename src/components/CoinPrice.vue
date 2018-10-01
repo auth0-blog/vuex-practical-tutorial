01d200602f58929fc14a1b6c3a8082baa0b10a3c
Bruno Krebs <bruno.krebs@auth0.com>
2018-10-01 10:29:17 -0400

    ### Defining your Vue.js Components

---
+++ src/components/CoinPrice.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="counter" class="bg-success text-white">
+    <div class="row p-3">
+      <div class="col-2">
+        <i class="fas fa-dollar-sign fa-4x"></i>
+      </div>
+      <div class="col-10 text-right">
+        <h2>{{format(price.amount)}}</h2>
+        ${{difference}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CoinPrice',
+    computed: {
+      price() {
+        return this.$store.getters.currentPrice;
+      },
+      difference() {
+        return this.$store.getters.difference;
+      }
+    },
+    methods: {
+      format: price => {
+        return price.toFixed(2);
+      }
+    }
+  };
+</script>
---
+++ src/components/PercentChange.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="counter" class="bg-primary text-white">
+    <div class="row p-3">
+      <div class="col-2"><i class="fas fa-percent fa-4x"></i></div>
+      <div class="col-10 text-right">
+        <h2>
+          {{percentageIncrease}}
+          <small>%</small>
+        </h2>
+        {{formatTimestamp(currentPrice.timestamp)}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PercentChange',
+    computed: {
+      percentageIncrease() {
+        return this.$store.getters.percentageIncrease;
+      },
+      currentPrice() {
+        return this.$store.getters.currentPrice;
+      }
+    },
+    methods: {
+      formatTimestamp: timestamp => {
+        const currentDate = new Date(timestamp);
+        return currentDate.toString().substring(16, 24);
+      }
+    }
+  };
+</script>
